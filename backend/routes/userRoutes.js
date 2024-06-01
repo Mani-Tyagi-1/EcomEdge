a32d2c9d5fc36fd0928190585966516061989c07
ManiTyagi <manityagi1919@gmail.com>
2024-06-02 01:26:33 +0530

    Created Login and Logout

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.route("/").post(createUser);
+router.route("/auth").post(loginUser);
+router.route("/logout").post(logoutCurrentUser);
 
 export default router;
