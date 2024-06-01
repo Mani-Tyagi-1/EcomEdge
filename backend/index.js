88ef25ce7c45ae7429f6f3d07283addf70c886f6
ManiTyagi <manityagi1919@gmail.com>
2024-06-01 15:19:19 +0530

    created newUser

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes );
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
