81b15247aed80d079ca3fa1b6a8298c39edc963f
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-06-01 17:18:31 +0530

    category routes, models and Category controller added

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,11 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/userRoutes.js";
 
 // Utils
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -17,6 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use("/api/users", userRoutes );
+app.use("/api/users", userRoutes);
+app.use("api/category", categoryRoutes);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
