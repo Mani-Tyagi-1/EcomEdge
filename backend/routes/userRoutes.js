2c401d42474791a7283be0147b076423be071b13
ManiTyagi <manityagi1919@gmail.com>
2024-06-03 01:03:58 +0530

    Completed User controller at backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,27 @@ import {
   createUser,
   loginUser,
   logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
 } from "../controllers/userController.js";
+
+import { authenticate,authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(createUser);
+router.route("/").post(createUser).get(authenticate,authorizeAdmin, getAllUsers);
 router.route("/auth").post(loginUser);
 router.route("/logout").post(logoutCurrentUser);
+
+router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
+
+// Admin Route 👇
+router.route('/:id').delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
 
 export default router;
