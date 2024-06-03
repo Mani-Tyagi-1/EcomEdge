9fd7741e8bd454037ade000595d33ace479da1b6
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-06-03 13:40:19 +0530

    Index and category routes updated

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/users", userRoutes);
-app.use("api/category", categoryRoutes);
+app.use("/api/category", categoryRoutes);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
