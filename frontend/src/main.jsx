8e0080092a3e0b6d650a6401d1b3b1b66c328e14
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-06-05 02:51:57 +0530

    Added Profile, Admin Route, UserList, CategoryList and Admin Dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,39 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
-import { Route, RouterProvider, createRoutesFromElements } from 'react-router'
-import { createBrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/features/store';
-import Profile from './pages/User/Profile.jsx';
-
-// Private Route
-import PrivateRoute from './components/PrivateRoute.jsx';
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/features/store";
+import PrivateRoute from "./components/PrivateRoute";
 
 // Auth
-import Login from './pages/Auth/Login.jsx'
-import Register from './pages/Auth/Register.jsx'
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
 
+import Profile from "./pages/User/Profile";
 
+import AdminRoute from "./pages/Admin/AdminRoute";
+import UserList from "./pages/Admin/UserList";
 
+import CategoryList from "./pages/Admin/CategoryList";
+import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />} >
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-      <Route path ='' element={<PrivateRoute />} >
-        <Route path='/profile' element={<Profile />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
       </Route>
 
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
+        {/* <Route path="dashboard" element={<AdminDashboard />} /> */}
+      </Route>
     </Route>
   )
-)
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}> 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <RouterProvider router={router} />
   </Provider>
-)
+);
