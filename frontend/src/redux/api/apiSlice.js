ef24ea77c502f4124421cded2d6b8d693015b7ce
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-06-07 00:59:23 +0530

    Category List Updated

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,12 +1,10 @@
-import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
 
-import { BASE_URL } from '../features/constants'
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
 export const apiSlice = createApi({
-    baseQuery,
-    tagTypes: ["Product", "Order", "User", "Category"],
-    endpoints: () => ({}),
-    
-})
-
+  baseQuery,
+  tagTypes: ["Product", "Order", "User", "Category"],
+  endpoints: () => ({}),
+});
